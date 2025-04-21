b7a5f938095a627da71c58df5a18c53e78947ced
rintu1034 <mishramatrujyoti@gmail.com>
2025-04-22 02:05:31 +0530

    Fix Inngest API route and debug issues

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,3 +1,9 @@
+// filepath: c:\Users\mishr\Desktop\QuickCart\app\api\inngest\route.js
 import { inngest } from "@/config/inngest";
+
+export const POST = async (req, res) => {
+    console.log("Incoming request to Inngest:", req.body);
+    return inngest.handleRequest(req, res);
+};import { inngest } from "@/config/inngest";
 
 export const POST = inngest.handleRequest;
